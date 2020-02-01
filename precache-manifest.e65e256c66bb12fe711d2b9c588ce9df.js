0a5e343fae26076cf2ed804a2f6c1a759cdb959c
Ummimath Farisha <farishaummimath@gmail.com>
2020-02-01 17:25:06 +0530

    Updates

---
+++ precache-manifest.e65e256c66bb12fe711d2b9c588ce9df.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bf6582cbf8e73b64325b44803848cd1b",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "2a8e0973b25747cdc528",
+    "url": "/portfolio/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "724f70080248f184b896",
+    "url": "/portfolio/static/js/2.f5bb07c4.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/portfolio/static/js/2.f5bb07c4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2a8e0973b25747cdc528",
+    "url": "/portfolio/static/js/main.ec822237.chunk.js"
+  },
+  {
+    "revision": "0d98471f6c3b69ca4251",
+    "url": "/portfolio/static/js/runtime-main.44087e9b.js"
+  }
+]);
